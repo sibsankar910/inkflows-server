87e84fd5404edaf923f682523790931a4ddb29fb
Sibsankar-de <sibsankarde910@gmail.com>
2024-08-29 09:36:35 +0530

    multer change

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,8 +1,17 @@
 import multer from "multer";
+import path from "path"
+import { fileURLToPath } from 'url';
+
+// Convert import.meta.url to a file path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// creates a temporary dir
+const tempDir = path.join(__dirname, '../..', 'public', 'temp');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './public/temp')
+        cb(null, tempDir)
     },
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
