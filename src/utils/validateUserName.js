b4ac1f2ebfa8053cb699ecaadaee7bf42acd43fb
Sibsankar-de <sibsankarde910@gmail.com>
2024-08-29 09:20:21 +0530

    Conflict change

---
+++ src/utils/validateUserName.js
@@ -0,0 +1,64 @@
+import axios from "axios";
+
+function containsForbiddenWords(username) {
+    const forbiddenWords = [
+        "admin", "root", "system",
+        "select", "drop", "insert", "delete",
+        "<script>", "alert", "kill", "suicide",
+        "murder", "nazi", "klan",
+        "f***", "s***", "b****", "c***",
+        "a******", "p***", "d***", "sex",
+        "porn", "xxx", "@example.com"
+    ];
+
+    // Normalize the username to lower case for case-insensitive comparison
+    const lowerUsername = username?.toLowerCase();
+
+    for (let word of forbiddenWords) {
+        if (lowerUsername?.includes(word)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+export async function validateUsername(username) {
+    const regex = /^[a-zA-Z0-9_-]{3,20}$/;
+    let isNameUseable = true, status = 200
+
+    if (!regex.test(username) || containsForbiddenWords(username)) {
+        isNameUseable = false
+        status = 401
+    } else {
+        isNameUseable = true
+        status = 200
+    }
+
+    // check for useravailable or not
+    var isUserAvailable = true
+    try {
+        await axios.get('/user/get-usernamelist')
+            .then((res) => {
+                const list = res?.data?.data
+                if (list?.includes(username)) {
+                    isUserAvailable = false
+                    status = 402
+                } else {
+                    isUserAvailable = true
+                    status = 200
+                }
+            })
+    } catch (error) {
+        console.log(error);
+    }
+
+    let isUserValidate = true
+    if (!isNameUseable || !isUserAvailable) {
+        isUserValidate = false
+    } else {
+        isUserValidate = true
+    }
+
+    return { status, isUserValidate }
+}
